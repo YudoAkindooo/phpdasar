5cf1567f36bb4073bc5acbd8eba51100ef517087
YudoAkindooo <143566389+YudoAkindooo@users.noreply.github.com>
2024-07-23 22:43:35 +0700

    Yudo Akindo IX RPL

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+$situs = "www.dunialkom.com";
+
+$belajar1 = 'sedang belajar programming di $situs';
+echo $belajar1;
+//sedang belajar programming di $situs
+
+echo "<br>";
+
+$belajar2 = "sedang belajar programming di $situs";
+echo $belajar2;
+// sedang belajar programming di www.dunialkom.com
